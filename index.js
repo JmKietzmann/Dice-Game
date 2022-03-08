904cc42f8419f925222dc020c0f202f72d3b8f2c
JmKietzmann <kietzmann.jm@gmail.com>
2022-03-08 19:55:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+var dice = [1,2,3,4,5,6]
+var diceOneSrc = document.getElementById("dice1");
+var diceTwoSrc = document.getElementById("dice2");
+
+function rollDice1() {
+    
+    var outcome =  Math.floor(Math.random() * 6);
+    var outcome1 =  Math.floor(Math.random() * 6);
+   
+if (outcome === 0) {
+   diceOneSrc.src = "Img/Side1.png";
+} else if (outcome === 1){
+    diceOneSrc.src = "Img/Side2.png";
+} else if (outcome === 2){
+    diceOneSrc.src = "Img/Side3.png";
+} else if (outcome === 3){
+    diceOneSrc.src = "Img/Side4.png";
+} else if (outcome === 4){
+    diceOneSrc.src = "Img/Side5.png";
+} else if (outcome === 5){
+    diceOneSrc.src = "Img/Side6.png";
+}
+   
+if (outcome1 === 0) {
+   diceTwoSrc.src = "Img/Side1.png";
+} else if (outcome1 === 1){
+    diceTwoSrc.src = "Img/Side2.png";
+} else if (outcome1 === 2){
+    diceTwoSrc.src = "Img/Side3.png";
+} else if (outcome1 === 3){
+    diceTwoSrc.src = "Img/Side4.png";
+} else if (outcome1 === 4){
+    diceTwoSrc.src = "Img/Side5.png";
+} else if (outcome1 === 5){
+    diceTwoSrc.src = "Img/Side6.png";
+}
+onWin(outcome, outcome1);
+};
+
+
+var player1 = outcome
+var player2 = outcome1
+
+function onWin(p1,p2){
+
+
+    if (p1===p2) {
+        document.getElementById("player1Notif").innerHTML = "Draw!";
+        document.getElementById("player2Notif").innerHTML = "Draw!"; 
+    } else if (p1 > p2) {
+        document.getElementById("player1Notif").innerHTML = "Winner!";
+        document.getElementById("player2Notif").innerHTML = "Loser!"; 
+    } else if (p1 < p2) {
+        document.getElementById("player1Notif").innerHTML = "Loser!";
+        document.getElementById("player2Notif").innerHTML = "Winner!"; 
+}};
